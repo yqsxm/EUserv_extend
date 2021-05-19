60069af97d9fa4461f05795b8de6708077303186
yqsxm <66452880+yqsxm@users.noreply.github.com>
2021-05-19 21:41:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -134,12 +134,12 @@ if __name__ == "__main__":
                     print("ServerID: %s Renew Error!" % k)
                 else:
                     print("ServerID: %s has been successfully renewed!" % k)
-                    print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID;true" % )
+#                    print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID;true" % )
                     curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s续签成功." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"
 #                    curl "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s续签成功"
             else:
                 print("ServerID: %s does not need to be renewed" % k)
-                print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID" % )
+#                print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID" % )
                 curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s目前不需续签." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"                           
 #                curl "https://api.telegram.org/bot${{TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s目前不需续签"
         time.sleep(15)
