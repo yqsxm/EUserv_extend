84275c6718469bb745dc3a96a9047ea33e046a75
yqsxm <66452880+yqsxm@users.noreply.github.com>
2021-05-19 21:17:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ PROXIES = {
     "http": "http://127.0.0.1:10809",
     "https": "http://127.0.0.1:10809"
 }
-
+print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID" % k)
 
 def login(username, password) -> (str, requests.session):
     headers = {
@@ -138,7 +138,7 @@ if __name__ == "__main__":
 #                    curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s续签成功." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"
 #                    curl "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s续签成功"
             else:
-                print("ServerID: %s does not need to be renewed;false" % k)
+                print("ServerID: %s does not need to be renewed" % k)
                 print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID" % k)
                 curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s目前不需续签." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"                           
 #                curl "https://api.telegram.org/bot${{TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s目前不需续签"
