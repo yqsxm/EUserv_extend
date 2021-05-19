2d2b1653495fe65c95f60b081f4304a79f61095e
yqsxm <66452880+yqsxm@users.noreply.github.com>
2021-05-19 22:14:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -139,8 +139,7 @@ if __name__ == "__main__":
 #                    curl "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s续签成功"
             else:
                 print("ServerID: %s does not need to be renewed." % k)
-                print(TELEGRAM_BOT_TOKEN ,"does not need to be renewed." % k)
-                print(TELEGRAM_CHAT_ID ,"does not need to be renewed." % k)
+                print(${{TELEGRAM_CHAT_ID }}"does not need to be renewed." % k)
 #                curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s目前不需续签." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"                           
 #                curl "https://api.telegram.org/bot${{TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s目前不需续签"
         time.sleep(15)
