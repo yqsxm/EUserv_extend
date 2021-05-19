eeb600d091071b23eca29da919fade2c07124db1
yqsxm <66452880+yqsxm@users.noreply.github.com>
2021-05-19 21:47:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -135,13 +135,13 @@ if __name__ == "__main__":
                 else:
                     print("ServerID: %s has been successfully renewed!" % k)
 #                    print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID;true" % )
-                    curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s续签成功." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"
-#                    curl "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s续签成功"
+ #                   curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s续签成功." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"
+                    curl "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s续签成功"
             else:
-                print("ServerID: %s does not need to be renewed" % k)
+                print("ServerID: %s TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID : %s  does not need to be renewed" % k)
 #                print("TELEGRAM_BOT_TOKEN : %s TELEGRAM_CHAT_ID" % )
-                curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s目前不需续签." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"                           
-#                curl "https://api.telegram.org/bot${{TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s目前不需续签"
+#                curl -s --data chat_id="${{TELEGRAM_CHAT_ID }}" --data "text=德鸡ServerID: %s目前不需续签." "https://api.telegram.org/bot${{ TELEGRAM_BOT_TOKEN }}/sendMessage"                           
+                curl "https://api.telegram.org/bot${{TELEGRAM_BOT_TOKEN }}/sendMessage" -d "chat_id=${{TELEGRAM_CHAT_ID }}&text=德鸡ServerID: %s目前不需续签"
         time.sleep(15)
         check(sessid, s)
         time.sleep(5)
